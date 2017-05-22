e15934eeb079cb8e4f5743b022fdc1a3b9d41a7f
Raed Felfel <Raed.Felfel@Gmail.com>
2017-05-22 20:06:35 +0300

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int Grade { get; set; }
+        public string FullName { get; set; }
+    }
+}
---
+++ StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        Student[] students = new Student[]
+        {
+            new Student {Id=1, Grade=1,FullName="Mohammad Ali" },
+            new Student {Id=2, Grade=1,FullName="Omar Habib" },
+            new Student {Id=3, Grade=1,FullName="Shadi Jamil" },
+            new Student {Id=4, Grade=2,FullName="Kamel bbadri" },
+            new Student {Id=5, Grade=2,FullName="Khali Ahmad" }
+        };
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            return students;
+        }
+
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = students.FirstOrDefault(std => std.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+    }
+}
